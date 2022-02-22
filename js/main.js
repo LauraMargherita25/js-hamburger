81cad3e8476f8f9a723e2a1a16b28590d9de5000
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-22 16:31:30 +0100

    first try

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let btnClose = document.querySelector("#close"); 
+let hiddenMenu = document.querySelector("#hidden_menu");
+
+btnClose.addEventListener("click", function(){
+   element.classList.toggle(hiddenMenu);
+})
