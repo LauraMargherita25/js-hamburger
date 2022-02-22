7009f38a02486899e46d78a368666d206658d584
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-22 19:09:37 +0100

    Made it wrong but at list it works

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let btnOpenMenu = document.querySelector(".fa-bars"); 
+let btnCloseMenu = document.querySelector(".close")
 let hiddenMenu = document.querySelector(".hamburger-menu");
 
 btnOpenMenu.addEventListener("click", function(){
     hiddenMenu.classList.add("show");
+})
+
+btnCloseMenu.addEventListener("click", function(){
+    hiddenMenu.classList.remove("show");
 })
