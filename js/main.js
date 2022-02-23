09b7abd07b81bd41202852396e59634ab1e5c1fa
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-23 10:13:11 +0100

    alternative solustion

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-let btnOpenMenu = document.querySelector(".fa-bars"); 
+let btnOpenMenu = document.querySelector(".header-right > a"); 
 let btnCloseMenu = document.querySelector(".close")
 let hiddenMenu = document.querySelector(".hamburger-menu");
 
 btnOpenMenu.addEventListener("click", function(){
     hiddenMenu.classList.add("active");
+//     hiddenMenu.style.display = "block";
 })
 
 btnCloseMenu.addEventListener("click", function(){
     hiddenMenu.classList.remove("active");
+    //   hiddenMenu.style.display = "none";
 })
 
